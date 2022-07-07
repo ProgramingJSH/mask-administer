c06dbfec24350086fb8fef6e46dc2c9a4366af2f
ProgramingJSH <programingjsh@gmail.com>
2022-07-07 20:10:06 +0900

    vsc

--- main.py
+++ main.py
@@ -6,14 +6,18 @@
 #     설정
 #     나가기
 # 각각의 기능은 함수로 나누고 메인함수에 모아보기.
-# 너무 큰 파일은 그냥 나눠서 한 다음 임포트해서 쓰자. 
+# 너무 큰 파일은 그냥 나눠서 한 다음 임포트해서 쓰자.
 
 def startMenu():
     print("*"*50)
     print("어서오세요! 마스크 종합관리 시스템입니다.")
+    print("숫자를 입력해주세요!")
     print()
     print("1. 시작하기")
     print("2. 로드")
+    print()
+    print("*"*50)
+
 
 def printMainMenu():
     print("*"*50)
@@ -25,6 +29,7 @@ def printMainMenu():
     print("4. 나가기")
     print()
     print("*"*50)
+
 
 def mainSelection():
     SelectNum = input("> ")
@@ -45,10 +50,12 @@ def mainSelection():
 
     return "continue"
 
+
 def main():
-    
+
     printMainMenu()
     while mainSelection() == "continue":
         printMainMenu()
-    
+
+
 main()
